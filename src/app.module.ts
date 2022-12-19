c69f626558b419e3ef23127b402ab7cff069220d
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2022-12-19 23:38:40 +0900

    :art: bounded Context별 모듈 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CocktailModule } from './cocktail/cocktail.module';
+import { UserModule } from './user/user.module';
+import { RecommendationModule } from './recommendation/recommendation.module';
+import { RefrigeratorModule } from './refrigerator/refrigerator.module';
+import path = require('path');
+import { ConfigModuleOption } from '../src/configs/env/ConfigModuleOption';
+const boundedContextModule = [
+  CocktailModule,
+  UserModule,
+  RecommendationModule,
+  RefrigeratorModule,
+];
+
+const datasourcePath = path.join(
+  __dirname,
+  '..',
+  'configs',
+  'env',
+  `.${process.env.NODE_ENV}.env`,
+);
 
 @Module({
-  imports: [],
+  imports: [
+    ...boundedContextModule,
+    ConfigModule.forRoot({
+      envFilePath: datasourcePath,
+      ...ConfigModuleOption,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
