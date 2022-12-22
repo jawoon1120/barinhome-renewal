c61b7feb7bf54dbc5ce622a1be855132d1bc658b
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2022-12-22 11:53:16 +0900

    :heavy_plus_sign: 유저 모듈 의존성

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './application/user.service';
+import { UserEntityMapper } from './infra/UserEntityMapper';
+import { UserRepository } from './infra/UserRepository';
 import { UserController } from './interface/user.controller';
 
 @Module({
+  imports: [],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserEntityMapper, UserRepository],
 })
 export class UserModule {}
