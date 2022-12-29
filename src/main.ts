a3376a42a93e7f50c139d530bcf8c8eb184b8298
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2022-12-29 15:13:01 +0900

    :sparkles: swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('BarInHome-Refactoring')
+    .setDescription('The BarInHome API description using DDD')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 bootstrap();
