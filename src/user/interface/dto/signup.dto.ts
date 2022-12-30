2f42af464b24439eb6e35a94d26a44efadc0f56c
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2022-12-30 23:58:29 +0900

    :sparkles: 회원가입 DTO 구현

---
+++ src/user/interface/dto/signup.dto.ts
@@ -0,0 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class SignUpPostDTO {
+  @ApiProperty({
+    example: 'barinhome@gmail.com',
+    description: '유저가 입력한 아이디',
+    required: true,
+    type: String,
+  })
+  @IsString()
+  userId: string;
+
+  @ApiProperty({
+    example: 'qkdlsgha12#$',
+    description: '유저가 입력한 비밀번호',
+    required: true,
+    type: String,
+  })
+  @IsString()
+  passWord: string;
+
+  @ApiProperty({
+    example: '김석봉',
+    description: '유저 이름',
+    required: true,
+
+    type: String,
+  })
+  @IsString()
+  name: string;
+}
+
+export class SignUpResDTO {
+  constructor(userId: string, result: string) {
+    this.userId = userId;
+    this.result = result;
+  }
+
+  @ApiProperty({
+    example: 'barinhome@gmail.com',
+    description: '유저가 입력한 아이디',
+    required: true,
+    type: String,
+  })
+  @IsString()
+  userId: string;
+
+  @ApiProperty({
+    example: '가입성공',
+    description: '회원가입 결과',
+    required: true,
+    type: String,
+  })
+  @IsString()
+  result: string;
+}
