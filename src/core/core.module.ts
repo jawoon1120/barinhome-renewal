13a4a64bba48103f889fcc6ab3453af0db3a5824
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2022-12-30 23:59:22 +0900

    :fire: core 모듈 삭제

--- src/core/core.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmDatabase } from './database/typeorm/TypeOrmDatabase';
-
-@Module({
-  providers: [TypeOrmDatabase],
-  exports: [TypeOrmDatabase],
-})
-export class CoreModule {}
